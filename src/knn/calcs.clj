b7c76b8ed71b014671a50ebfedb9b2d5b5bf4406
Michel Perez <michel.ingesoft@gmail.com>
2017-10-13 09:19:41 -0500

    wip

--- src/knn/calcs.clj
+++ src/knn/calcs.clj
@@ -3,20 +3,20 @@
 (defn euclidean-distance
   [vec1 vec2]
   (->>
-    (map #(Math/pow (- %1 %2) 2) vec1 vec2)
-    (reduce +)
-    (Math/sqrt)))
+   (map #(Math/pow (- %1 %2) 2) vec1 vec2)
+   (reduce +)
+   (Math/sqrt)))
 
 (defn nearest-neighbors
   [samples query k]
   (->>
-    samples
-    (map #(assoc % :score (euclidean-distance query (:pos %))))
-    (sort-by :score)
-    (take k)))
+   samples
+   (map #(assoc % :score (euclidean-distance query (:pos %))))
+   (sort-by :score)
+   (take k)))
 
 (defn knn
   [samples query k]
   (let [votes (nearest-neighbors samples query k)
         vote-freq (frequencies (map :class votes))]
-       (key (apply max-key val vote-freq))))
+    (key (apply max-key val vote-freq))))
